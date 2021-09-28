44a8fd940b793e3cd6850e4d7adc40fcb0678b9b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-09-28 11:38:52 +0200

    (craftr-dsl) bump version to 0.4.2

--- src/craftr/dsl/__init__.py
+++ src/craftr/dsl/__init__.py
@@ -4,7 +4,7 @@ This package implements the Craftr DSL laguage.
 """
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 import typing as t
 from .rewrite import SyntaxError
