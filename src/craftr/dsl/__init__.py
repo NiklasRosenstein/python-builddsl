bc4ef061546972a0c27e85772300012f4bb0f1db
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-02-22 01:06:00 +0100

    release 0.7.4

--- src/craftr/dsl/__init__.py
+++ src/craftr/dsl/__init__.py
@@ -6,7 +6,7 @@ from ._rewriter import Grammar, SyntaxError
 from ._runtime import ChainContext, Closure, Context, MapContext, ObjectContext
 from ._transpiler import TranspileOptions, transpile_to_ast, transpile_to_source
 
-__version__ = '0.7.3'
+__version__ = '0.7.4'
 
 __all__ = [
   'execute',
