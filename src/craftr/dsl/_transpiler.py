78cee2fa3a9b9398be2032ae3e6c45de01d50237
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-04-20 23:54:51 +0200

    feature: Expose `NameRewriter` functionality as a a public API under the name `craftr.dsl.ast_utils.DynamicLookupRewriter`

--- src/craftr/dsl/_transpiler.py
+++ src/craftr/dsl/_transpiler.py
@@ -10,6 +10,7 @@ from contextlib import contextmanager
 from dataclasses import dataclass, field
 
 from ._rewriter import Closure, Grammar, Rewriter
+from .ast_utils import DynamicLookupRewriter
 
 
 @dataclass
@@ -70,7 +71,8 @@ def transpile_to_ast(code: str, filename: str, options: t.Optional[TranspileOpti
     module = ast.parse(rewrite.code, filename, mode='exec', type_comments=False)
   module = ClosureRewriter(filename, options, rewrite.closures).visit(module)
   if options.closure_target:
-    module = t.cast(ast.Module, NameRewriter(options).visit(module))
+    dynamic_lookup = DynamicLookupRewriter(options.closure_target, options.pure_builtins, options.local_vardef_prefix)
+    module = t.cast(ast.Module, dynamic_lookup.visit(module))
   return ast.fix_missing_locations(module)
 
 
@@ -159,111 +161,5 @@ class ClosureRewriter(ast.NodeTransformer):
           func = self.visit(self._get_closure_def(closure_id))
           result.insert(len(result) - 1, func)
       return result
-    finally:
-      assert self._hierarchy.pop() == node
-
-
-class NameRewriter(ast.NodeTransformer):
-  """ Rewrites names be accessed through a global object. """
-
-  def __init__(self, options: TranspileOptions) -> None:
-    self.options = options
-    self._hierarchy: t.List[ast.AST] = []
-    self._defined_locals: t.List[t.Set[str]] = [set()]
-
-  def _add_to_locals(self, varnames: t.Set[str]) -> None:
-    assert self._defined_locals, 'no locals in current scope'
-    self._defined_locals[-1].update(varnames)
-
-  @contextmanager
-  def _with_locals(self, varnames: t.Set[str]) -> t.Iterator[None]:
-    self._defined_locals.append(varnames)
-    try:
-      yield
-    finally:
-      self._defined_locals.pop()
-
-  @contextmanager
-  def _with_locals_from_target(self, target: ast.expr) -> t.Iterator[None]:
-    names: t.Set[str] = set()
-    if isinstance(target, ast.Name):
-      names.add(target.id)
-    elif isinstance(target, (ast.List, ast.Tuple)):
-      names.update(n.id for n in target.elts)  # type: ignore  # TODO (@NiklasRosenstein)
-    else:
-      raise TypeError(f'expected Name/List/Tuple, got {type(target).__name__}')
-    with self._with_locals(names):
-      yield
-
-  def _has_local(self, varname: str) -> bool:
-    if self._defined_locals:
-      return varname in self._defined_locals[-1]
-    return False
-
-  def _has_nonlocal(self, varname: str) -> bool:
-    for locals in self._defined_locals:
-      if varname in locals:
-        return True
-    return varname == self.options.closure_target or varname in self.options.pure_builtins
-
-  def visit_Name(self, node: ast.Name) -> ast.AST:
-    if self._has_nonlocal(node.id):
-      return node
-    return ast.Subscript(value=ast.Name(id=self.options.closure_target, ctx=ast.Load()),
-                         slice=ast.Index(value=ast.Constant(value=node.id)),
-                         ctx=node.ctx)
-
-  def visit_Assign(self, assign: ast.Assign) -> ast.AST:
-    if len(assign.targets) == 1 and isinstance(assign.targets[0], ast.Name):
-      name = assign.targets[0]
-      if name.id.startswith(self.options.local_vardef_prefix):
-        name.id = name.id[len(self.options.local_vardef_prefix):]
-        self._add_to_locals({name.id})
-    return self.generic_visit(assign)
-
-  def visit_For(self, node: ast.For) -> ast.AST:
-    with self._with_locals_from_target(node.target):
-      return self.generic_visit(node)
-
-  def visit_FunctionDef(self, node: ast.FunctionDef) -> ast.AST:
-    self._add_to_locals({node.name})
-    names: t.Set[str] = set()
-    for arg in node.args.args:
-      names.add(arg.arg)
-    for arg in node.args.kwonlyargs:
-      names.add(arg.arg)
-    if node.args.vararg:
-      names.add(node.args.vararg.arg)
-    if node.args.kwarg:
-      names.add(node.args.kwarg.arg)
-    with self._with_locals(names):
-      return self.generic_visit(node)
-
-  def visit_ClassDef(self, node: ast.ClassDef) -> ast.AST:
-    self._add_to_locals({node.name})
-    return self.generic_visit(node)
-
-  def visit_Import(self, node: ast.Import) -> ast.AST:
-    names: t.Set[str] = set()
-    for name in node.names:
-      if name.asname:
-        names.add(name.asname)
-      elif '.' in name.name:
-        names.add(name.name.rpartition('.')[0])
-      else:
-        names.add(name.name)
-    self._add_to_locals(names)
-    return self.generic_visit(node)
-
-  def visit_ImportFrom(self, node: ast.ImportFrom) -> ast.AST:
-    self.visit_Import(ast.Import(names=node.names))  # Dispatch name detection
-    return self.generic_visit(node)
-
-  # TODO(NiklasRosenstein): Handle more nodes that define local variables and := operator.
-
-  def visit(self, node: ast.AST) -> ast.AST:
-    self._hierarchy.append(node)
-    try:
-      return super().visit(node)
     finally:
       assert self._hierarchy.pop() == node
